993db6b0ccc5045acf546caea57994c66a73cdf5
Arjun123sh <arjunrocks405@gmail.com>
2025-07-24 15:12:57 +0530

    Added firebase sdk

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,61 +1,125 @@
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatefulWidget{
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
+
   @override
   State<AuthScreen> createState() {
-
     return _AuthScreenState();
   }
 }
 
-class _AuthScreenState extends State<AuthScreen>{
+class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+
   @override
   Widget build(BuildContext context) {
+    void _submit() {
+      final isValid = _form.currentState!.validate();
+      if (isValid) {
+        _form.currentState!.save();
+      }
+    }
+
 
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Chat' ,
-        style: Theme.of(context).textTheme.titleLarge) ,
-        backgroundColor: Color.fromARGB(255, 166, 81, 204),) ,
+      appBar: AppBar(
+        title: Text(
+          'Flutter Chat',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        backgroundColor: Color.fromARGB(255, 166, 81, 204),
+      ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               margin: const EdgeInsets.only(
-                top : 30 ,
-                bottom: 20 ,
-                left : 20 ,
-                right: 20
+                top: 30,
+                bottom: 20,
+                left: 20,
+                right: 20,
               ),
               width: 200,
-              child: Image.asset('assets/images/chat.png'),
-            ) ,
-            Card(margin: const EdgeInsets.all(20),
-            child: SingleChildScrollView(
-              child: Padding(padding: const EdgeInsets.all(16) ,
-              child: Form(child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Email Address'
+              child: Image.asset('/home/karan/AndroidStudioProjects/flutter_chat/lib/chat.png'),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Form(
+                    key: _form,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Email Address',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@')) {
+                              return 'Please enter a valid email address';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _enteredEmail = value!;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                          ),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be 6 characters long';
+                            }
+                            return null;
+                          },
+                            onSaved: (value) {
+                              _enteredPassword = value!;
+                            }
+                        ),
+                        const SizedBox(height: 12),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme.of(
+                              context,
+                            ).colorScheme.primaryContainer,
+                          ),
+                          onPressed: _submit,
+                          child: Text(_isLogin ? 'Login' : 'Signup'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(
+                            _isLogin
+                                ? 'Create an account'
+                                : 'I already have an account.',
+                          ),
+                        ),
+                      ],
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                    autocorrect: false,
-                    textCapitalization: TextCapitalization.none,
-                  ) ,
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Password'
-                    ),
-                    obscureText: true,
-
-                  )
-                ],
-              )),),
-            ),)
-
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
